eb907d7a4372d316cb91e116507113555104ca98
Mike Dingjan <mike@mikedingjan.nl>
2013-05-24 23:12:06 +0200

    run not inner_run

--- project_name/project_name/management/commands/collectstatic.py
+++ project_name/project_name/management/commands/collectstatic.py
@@ -2,15 +2,17 @@ import os
 
 from django.conf import settings
 from django.contrib.staticfiles.management.commands.collectstatic import Command as CollectstaticCommand
+from django.core.management import CommandError
 
 
 class Command(CollectstaticCommand):
     """
     Make sure the media dir exists before we're running collecstatic.
     """
-    def inner_run(self, *args, **options):
+    def run(self, *args, **options):
         if not os.path.exists(settings.MEDIA_ROOT):
             os.makedirs(settings.MEDIA_ROOT)
-        super(Command, self).inner_run(*args, **options)
+
+        super(Command, self).run(*args, **options)
 
 
